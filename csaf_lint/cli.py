fb7eb4720990625e5c27b10df4da5c7464b4119e
Stefan Hagen <stefan@hagen.link>
2021-03-07 10:55:31 +0100

    Update cli.py

--- csaf_lint/cli.py
+++ csaf_lint/cli.py
@@ -1,3 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
+""""Visit folder tree with CSAF or CVRF documents, validate the latter, and generate reports."""
+import os
+import sys
+
+import csaf_lint.lint as ag
+
+
+# pylint: disable=expression-not-assigned
+def main(argv=None):
+    """Process the job."""
+    argv = sys.argv[1:] if argv is None else argv
+    ag.main(argv)
+    
