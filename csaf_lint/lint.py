39f0b68c3833926f195c52a89330b6f685bf7836
Stefan Hagen <stefan@hagen.link>
2021-03-10 00:03:25 +0100

    Initial cli mock implementation

--- csaf_lint/lint.py
+++ csaf_lint/lint.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=expression-not-assigned,line-too-long
+"""Visit CSAF/CVRF files and validate them against envelope (core) and given body profiles."""
+import copy
+import csv
+import datetime as dti
+import hashlib
+import lzma
+import os
+import pathlib
+import subprocess
+import sys
+
+DEBUG_VAR = "AG_DEBUG"
+DEBUG = os.getenv(DEBUG_VAR)
+
+
+def main(argv=None):
+    """Drive the validator."""
+    argv = argv if argv else sys.argv[1:]
+    if not argv:
+        print("ERROR arguments expected.", file=sys.stderr)
+        return 2
