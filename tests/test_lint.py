afbf1d5d5c40a526e03fe8078a13bd095b009df3
Stefan Hagen <stefan@hagen.link>
2021-03-19 17:04:47 +0100

    Added real linter tests

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,4 +1,67 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
 import pathlib
+
+import jsonschema
 import pytest  # type: ignore
+
+import csaf_lint.cli as cli
+
+A_SCHEMA_PATH = pathlib.Path('csaf_lint', 'schema', 'csaf', '2.0', 'csaf.json')
+CONTENT_FEATURES = ('document', 'document-product', 'document-vulnerability', 'full', 'spam')
+USAGE_ERROR_TOKENS = ('requires', 'two', 'schema', 'document')
+
+
+def test_main_validate_spam_ok(capsys):
+    """
+    python -m csaf_lint csaf_lint/schema/csaf/2.0/csaf.json tests/fixtures/csaf-2.0/baseline/spam/01.json
+    returns 0 and no additional information
+    """
+    for n in range(1, 11):
+        nn = f'{n:02d}'
+        a_document_path = pathlib.Path('tests', 'fixtures', 'csaf-2.0/', 'baseline', 'spam', f'{nn}.json')
+        argv = [A_SCHEMA_PATH, a_document_path]
+        assert cli.main(argv=argv) == 0
+        out, err = capsys.readouterr()
+        assert not out
+        assert not err
+
+
+def test_main_validate_spam_nok():
+    """
+    python -m csaf_lint csaf_lint/schema/csaf/2.0/csaf.json tests/fixtures/csaf-2.0/invalid/spam/01.json
+    returns 1 and plenty of information
+    """
+    a_document_path = pathlib.Path('tests', 'fixtures', 'csaf-2.0/', 'invalid', 'spam', '01.json')
+    argv = [A_SCHEMA_PATH, a_document_path]
+    message = r"'csaf_version' is a required property"
+    with pytest.raises(jsonschema.exceptions.ValidationError, match=message):
+        cli.main(argv=argv)
+
+
+def test_main_validate_rest_ok(capsys):
+    for content in CONTENT_FEATURES[:-1]:
+        for n in range(1, 11):
+            nn = f'{n:02d}'
+            a_document_path = pathlib.Path('tests', 'fixtures', 'csaf-2.0/', 'baseline', content, f'{nn}.json')
+            argv = [A_SCHEMA_PATH, a_document_path]
+            try:
+                assert cli.main(argv=argv) == 0
+            except jsonschema.exceptions.ValidationError as err:
+                raise ValueError(f"failed validation for {a_document_path} in {test_main_validate_rest_ok}. Details: {err}")
+            out, err = capsys.readouterr()
+            assert not out
+            assert not err
+
+
+def test_main_validate_rest_nok():
+    for content in CONTENT_FEATURES[:-1]:
+        for n in range(1, 11):
+            nn = f'{n:02d}'
+            a_document_path = pathlib.Path('tests', 'fixtures', 'csaf-2.0/', 'invalid', content, f'{nn}.json')
+            argv = [A_SCHEMA_PATH, a_document_path]
+            try:
+                cli.main(argv=argv)
+                raise ValueError(f"failed validation for {a_document_path} in {test_main_validate_rest_ok}.")
+            except jsonschema.exceptions.ValidationError:
+                pass
