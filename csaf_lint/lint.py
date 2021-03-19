2f8ad62b2f0b62f921cabaafc100e4968a4f6e59
Stefan Hagen <stefan@hagen.link>
2021-03-19 19:13:04 +0100

    Added more use cases: 2, 1, and 0 argument modes

--- csaf_lint/lint.py
+++ csaf_lint/lint.py
@@ -16,8 +16,16 @@ import jsonschema  # type: ignore
 
 ENCODING = "utf-8"
 
+CSAF_2_0_SCHEMA_PATH = pathlib.Path('csaf_lint', 'schema', 'csaf', '2.0', 'csaf.json')
+
 DEBUG_VAR = "CSL_DEBUG"
 DEBUG = os.getenv(DEBUG_VAR)
+
+
+def read_stdin():
+    """Create JSON object from stdin data."""
+    in_memory = [line for line in sys.stdin]
+    return json.loads(''.join(in_memory))
 
 
 def load(file_path):
@@ -35,11 +43,19 @@ def validate(document, schema, conformance=None):
 def main(argv=None, embedded=False):
     """Drive the validator."""
     argv = argv if argv else sys.argv[1:]
-    if len(argv) != 2:
-        print("Validation requires two positional arguments: schema.json document.json")
+    if len(argv) > 2:  # Unclear what the inputs beyond two may be
+        print("Usage: csaf-lint [schema.json] document.json")
+        print("   or: csaf-lint < document.json")
         return 2
 
-    schema = json.loads(argv[0]) if embedded else load(argv[0])
-    document = json.loads(argv[1]) if embedded else load(argv[1])
+    if len(argv) == 2:  # Schema file path is first
+        schema = json.loads(argv[0]) if embedded else load(argv[0])
+        document = json.loads(argv[1]) if embedded else load(argv[1])
+    else:
+        schema = load(CSAF_2_0_SCHEMA_PATH)
+        if len(argv) == 1:  # Assume schema implicit, argument given is document file path
+            document = load(argv[0])
+        else:
+            document = read_stdin()
 
     return 0 if validate(document, schema) is None else 1
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import io
 import pathlib
 
+import jsonschema
 import pytest  # type: ignore
 
 import csaf_lint.cli as cli
 
 A_SCHEMA_PATH = pathlib.Path('csaf_lint', 'schema', 'csaf', '2.0', 'csaf.json')
 CONTENT_FEATURES = ('document', 'document-product', 'document-vulnerability', 'full', 'spam')
-USAGE_ERROR_TOKENS = ('requires', 'two', 'schema', 'document')
+USAGE_ERROR_TOKENS = ('Usage:', 'schema', 'document', 'or:')
 
 
-def test_main_nok_empty_(capsys):
-    assert cli.main([]) == 2
+def test_main_nok_more_than_two_args(capsys):
+    assert cli.main([1, 2, 3]) == 2
     out, err = capsys.readouterr()
     for term in USAGE_ERROR_TOKENS:
         assert term in out
 
 
-def test_main_nok_int(capsys):
-    assert cli.main([42]) is 2
-    out, err = capsys.readouterr()
-    for term in USAGE_ERROR_TOKENS:
-        assert term in out
+def test_main_nok_read_empty_json_object_from_stdin(monkeypatch):
+    monkeypatch.setattr('sys.stdin', io.StringIO('{}'))
+    message = r"'document' is a required property"
+    with pytest.raises(jsonschema.exceptions.ValidationError, match=message):
+        cli.main([])
 
 
 def test_main_nok_int_(capsys):
