0231079a23c99022e2a75372952a1c5c4f53a533
Stefan Hagen <stefan@hagen.link>
2021-03-20 14:05:01 +0100

    Added experiemntal CVRF default 1.2 validation - still fails on the examples a and e

--- csaf_lint/lint.py
+++ csaf_lint/lint.py
@@ -13,6 +13,8 @@ import subprocess
 import sys
 
 import jsonschema  # type: ignore
+from lxml import etree  # type: ignore
+
 
 ENCODING = "utf-8"
 
@@ -28,16 +30,22 @@ CVRF_OASIS_ROOT = 'http://docs.oasis-open.org/csaf/csaf-cvrf/'
 CVRF_DEFAULT_SCHEMA = f'{CVRF_OASIS_ROOT}v{CRVF_DEFAULT_SEMANTIC_VERSION}/cs01/schemas/cvrf.xsd'
 CVRF_DEFAULT_NAMESPACES = {part.upper(): '{{{CVRF_OASIS_ROOT}v{CRVF_DEFAULT_SEMANTIC_VERSION}/{part}}}' for part in CVRF_PARTS}
 
-CVRF_DEFAULT_CATALOG = f'schemata/catalog_{CRVF_DEFAULT_SEMANTIC_VERSION.replace(".", "_")}.xml'
-CVRF_DEFAULT_SCHEMA_FILE = f'schemata/cvrf/{CRVF_DEFAULT_SEMANTIC_VERSION}/cvrf.xsd'
+CVRF_DEFAULT_CATALOG = f'schema/catalog_{CRVF_DEFAULT_SEMANTIC_VERSION.replace(".", "_")}.xml'
+CVRF_DEFAULT_SCHEMA_FILE = f'schema/cvrf/{CRVF_DEFAULT_SEMANTIC_VERSION}/cvrf.xsd'
 
 CVRF_PRE_OASIS_ROOT = 'http://www.icasi.org/CVRF/schema/cvrf/'
 
 CVRF_PRE_OASIS_SCHEMA = f'{CVRF_PRE_OASIS_ROOT}{CRVF_PRE_OASIS_SEMANTIC_VERSION}/cs01/schemas/cvrf.xsd'
 CVRF_PRE_OASIS_NAMESPACES = {part.upper(): '{{{CVRF_OASIS_ROOT}v{CRVF_DEFAULT_SEMANTIC_VERSION}/{part}}}' for part in CVRF_PARTS}
 
-CVRF_PRE_OASIS_CATALOG = f'schemata/catalog_{CRVF_PRE_OASIS_SEMANTIC_VERSION.replace(".", "_")}.xml'
-CVRF_PRE_OASIS_SCHEMA_FILE = f'schemata/cvrf/{CRVF_PRE_OASIS_SEMANTIC_VERSION}/cvrf.xsd'
+CVRF_PRE_OASIS_CATALOG = f'schema/catalog_{CRVF_PRE_OASIS_SEMANTIC_VERSION.replace(".", "_")}.xml'
+CVRF_PRE_OASIS_SCHEMA_FILE = f'schema/cvrf/{CRVF_PRE_OASIS_SEMANTIC_VERSION}/cvrf.xsd'
+
+CVRF_OASIS_NS_ROOT = 'http://docs.oasis-open.org/csaf/ns/csaf-cvrf/'
+CVRF_VERSION_NS_MAP = {
+    '1.2': f'{CVRF_OASIS_NS_ROOT}v1.2/cvrf',
+    '1.1': f'{CVRF_OASIS_NS_ROOT}v1.1/cvrf',
+}
 
 DEBUG_VAR = "CSL_DEBUG"
 DEBUG = os.getenv(DEBUG_VAR)
@@ -57,8 +65,111 @@ def load(file_path):
 
 def validate(document, schema, conformance=None):
     """Validate the document against the schema."""
-    conformance = conformance if conformance else jsonschema.draft7_format_checker
-    return jsonschema.validate(document, schema, format_checker=conformance)
+    if str(document).endswith('json'):
+        conformance = conformance if conformance else jsonschema.draft7_format_checker
+        return jsonschema.validate(document, schema, format_checker=conformance)
+    xml_tree, message = load_xml(document)
+    found, version, ns = versions_xml(xml_tree, CRVF_DEFAULT_SEMANTIC_VERSION)
+    status, message = xml_validate(schema, CVRF_DEFAULT_CATALOG, xml_tree, CRVF_DEFAULT_SEMANTIC_VERSION)
+    return None if status else 1
+
+
+def load_xml(document_path):
+    """
+    First things first: parse the document (to ensure it is well-formed XML) to obtain an ElementTree object
+    to pass to the CVRF validator/parser
+    """
+    try:
+        cvrf_doc = etree.parse(document_path, etree.XMLParser(encoding="utf-8"))  # "utf-8"
+    except IOError as err:
+        return None, f"file {document_path} failed with IO error {err}"
+    except etree.XMLSyntaxError as err:
+        return None, f"parsing from {document_path} failed with XMLSyntaxError error {err}"
+
+    return cvrf_doc, f"well-formed xml tree from {document_path}"
+
+
+def derive_version_from_namespace(root):
+    """Version detection of XML document per element tree object root."""
+    not_found = '', None
+    if root is None:
+        return not_found
+
+    mandatory_element = 'DocumentType'
+    for version, ns in CVRF_VERSION_NS_MAP.items():
+        token = '{%s}%s' % (ns, mandatory_element)
+        if root.find(token) is not None:
+            return version, ns
+
+    return not_found
+
+
+def versions_xml(xml_tree, request_version):
+    """Versions from cvrf namespace in xml tree and request version."""
+    sem_ver, doc_cvrf_version = derive_version_from_namespace(xml_tree.getroot())
+    req_cvrf_version = "http://docs.oasis-open.org/csaf/ns/csaf-cvrf/v1.2/cvrf"
+
+    if doc_cvrf_version:
+        return (True if doc_cvrf_version == req_cvrf_version else False), doc_cvrf_version, req_cvrf_version
+
+    return False, None, req_cvrf_version
+
+
+def cvrf_validate(f, xml_tree):
+    """
+    Validates a CVRF document
+
+    f: file object containing the schema
+    cvrf_doc: the serialized CVRF ElementTree object
+    returns: a code (True for valid / False for invalid) and a reason for the code
+    """
+    try:
+        xmlschema_doc = etree.parse(f)
+    except etree.XMLSyntaxError as err:
+        return False, f'Parsing error, schema document "{f.name}" is not well-formed: {err}'
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    try:
+        xmlschema.assertValid(xml_tree)
+        return True, "Valid"
+    except etree.DocumentInvalid:
+        return False, xmlschema.error_log
+
+
+def xml_validate(schema, catalog, xml_tree, request_version):
+    """Validate xml tree against given xml schema of request version assisted by catalog."""
+    fallback_catalog = CVRF_DEFAULT_CATALOG
+    if request_version != CRVF_DEFAULT_SEMANTIC_VERSION:
+        fallback_catalog = CVRF_PRE_OASIS_CATALOG
+    catalog = catalog if catalog else fallback_catalog
+    try:
+        if schema:
+            # Try to use local schema files
+            f = open(schema, 'r')
+
+            # If the supplied file is not a valid catalog.xml or doesn't exist lxml will fall back to using remote validation
+            os.environ.update(XML_CATALOG_FILES=catalog)
+        else:
+            # try to use local schema file
+            fallback_schema = CVRF_DEFAULT_SCHEMA_FILE
+            if request_version != CRVF_DEFAULT_SEMANTIC_VERSION:
+                fallback_schema = CVRF_PRE_OASIS_SCHEMA_FILE
+            schema = fallback_schema
+            f = open(schema, 'r')
+
+            catalog = catalog if catalog else fallback_catalog
+            os.environ.update(XML_CATALOG_FILES=catalog)
+
+    except IOError as err:
+        return False, f"validation of {xml_tree} against {schema} failed with IO error: {err}"
+
+    code, result = cvrf_validate(f, xml_tree)
+    f.close()
+
+    if code is False:
+        return False, f"validation of {xml_tree} against {schema} failed with error: {result}"
+    else:
+        return True, f"validation of {xml_tree} against {schema} succeeded with result: {result}"
 
 
 def main(argv=None, embedded=False):
@@ -69,14 +180,31 @@ def main(argv=None, embedded=False):
         print("   or: csaf-lint < document.json")
         return 2
 
-    if len(argv) == 2:  # Schema file path is first
-        schema = json.loads(argv[0]) if embedded else load(argv[0])
-        document = json.loads(argv[1]) if embedded else load(argv[1])
-    else:
-        schema = load(CSAF_2_0_SCHEMA_PATH)
-        if len(argv) == 1:  # Assume schema implicit, argument given is document file path
-            document = load(argv[0])
+    if len(argv) and argv[-1].endswith('json'):
+        if len(argv) == 2:  # Schema file path is first
+            schema = json.loads(argv[0]) if embedded else load(argv[0])
+            document = json.loads(argv[1]) if embedded else load(argv[1])
         else:
-            document = read_stdin()
+            schema = load(CSAF_2_0_SCHEMA_PATH)
+            if len(argv) == 1:  # Assume schema implicit, argument given is document file path
+                document = load(argv[0])
+            else:
+                document = read_stdin()
+    elif len(argv) and argv[-1].endswith('xml'):
+        if embedded:
+            print("Usage: csaf-lint [schema.xsd] document.xml")
+            print(" note: no embedding supported for xsd/xml")
+            return 2
+        if len(argv) == 2:  # Schema file path is first
+            schema = argv[0]
+            document = argv[1]
+        else:
+            schema = CVRF_DEFAULT_SCHEMA_FILE
+            if len(argv) == 1:  # Assume schema implicit, argument given is document file path
+                document = argv[0]
+            else:
+                print("Usage: csaf-lint [schema.xsd] document.xml")
+                print(" note: no embedding supported for xsd/xml")
+                return 2
 
     return 0 if validate(document, schema) is None else 1
