db8629a5d2c7165de7a5f32c12d97b892b72d83c
Stefan Hagen <stefan@hagen.link>
2021-03-21 15:29:24 +0100

    Added less clumsy inject in cli main for debug when usng the API

--- csaf_lint/cli.py
+++ csaf_lint/cli.py
@@ -13,13 +13,14 @@ DEBUG = os.getenv(DEBUG_VAR)
 
 
 # pylint: disable=expression-not-assigned
-def main(argv=None):
+def main(argv=None, debug=False):
     """Dispatch processing of the job.
     This is the strings only command line interface.
     For python API use interact with lint functions directly.
     """
     argv = sys.argv[1:] if argv is None else argv
     embedded = False
+    debug = debug if debug else DEBUG
     for arg in argv:
         if not pathlib.Path(arg).is_file():
             if not embedded:
@@ -28,4 +29,4 @@ def main(argv=None):
                 print("ERROR: embedding only works for none or all.")
                 sys.exit(2)
 
-    return lint.main(argv, embedded=embedded, debug=DEBUG)
+    return lint.main(argv, embedded=embedded, debug=debug)
