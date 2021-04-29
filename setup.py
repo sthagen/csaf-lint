42cf05d387fed21ffe975cef43d04bbd056a6e18
Stefan Hagen <stefan@hagen.link>
2021-04-29 21:52:16 +0200

    Added support for python 3.7

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Operating System :: Unix",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Internet",
