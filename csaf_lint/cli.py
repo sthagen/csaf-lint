a2ed306f78b1daadb15e66a5ca7cfa125a30ddfa
Stefan Hagen <stefan@hagen.link>
2021-05-08 11:41:26 +0200

    Removed stray double quote from module docstring

--- csaf_lint/cli.py
+++ csaf_lint/cli.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
-""""Visit folder tree with CSAF or CVRF documents, validate the latter, and generate reports."""
+"""Visit folder tree with CSAF or CVRF documents, validate the latter, and generate reports."""
 import pathlib
 import os
 import sys
