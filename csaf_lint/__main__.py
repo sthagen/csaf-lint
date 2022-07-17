a85d916bf037f83497d115c83991eb4e490bc2da
Stefan Hagen <stefan@hagen.link>
2022-07-17 17:40:46 +0200

    Happy linter and simplified

--- csaf_lint/__main__.py
+++ csaf_lint/__main__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
 import sys
 
 from csaf_lint.cli import main
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))  # pragma: no cover
