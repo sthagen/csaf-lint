f6fc53853ab82c6ee537bf49571094164170c6d5
Stefan Hagen <stefan@hagen.link>
2022-12-28 16:03:54 +0100

    Updated license stuff

--- gen_licenses.py
+++ gen_licenses.py
@@ -5,7 +5,10 @@ import pathlib
 import pkg_resources
 import string
 import subprocess  # nosec
+import sys
 from typing import List, Tuple
+
+import yaml
 
 __all__ = ['dependency_tree_console_text', 'direct_dependencies_table', 'indirect_dependencies_table']
 
@@ -14,9 +17,42 @@ TP_PATH = pathlib.Path('docs', 'third-party')
 
 TABLE_KEYS = (('Name', 'URL'), 'Version', 'License', 'Author', 'Description')
 HEADER_LABELS = ('Name', 'Version', 'License', 'Author', 'Description (from packaging data)')
-FALLBACK_URLS = {
-    'typing-extensions': 'https://github.com/python/typing/blob/master/typing_extensions/README.rst',
-}
+
+fallbacks, FALLBACK_URLS, FALLBACK_AUTHORS, FALLBACK_DESCRIPTIONS = {}, {}, {}, {}
+
+THIRD_PARTY_FALLBACKS = 'third-party-fallbacks.yml'
+TPF_PATH = pathlib.Path(THIRD_PARTY_FALLBACKS)
+if TPF_PATH.is_file():
+    print(f'Reading fallback values from file ({TPF_PATH})', file=sys.stderr)
+    with open(TPF_PATH, 'rt', encoding=ENCODING) as handle:
+        fallbacks = yaml.safe_load(handle)
+
+if fallbacks:
+    if fallbacks.get('urls', {}):
+        FALLBACK_URLS = {**FALLBACK_URLS, **fallbacks['urls']}
+    if fallbacks.get('authors', {}):
+        FALLBACK_AUTHORS = {**FALLBACK_AUTHORS, **fallbacks['authors']}
+    if fallbacks.get('descriptions', {}):
+        FALLBACK_DESCRIPTIONS = {**FALLBACK_DESCRIPTIONS, **fallbacks['descriptions']}
+
+indirect_names, INDIRECT_NAMES = [], []
+
+INDIRECT_PACKAGE_NAMES = 'indirect-package-names.yml'
+IPN_PATH = pathlib.Path(INDIRECT_PACKAGE_NAMES)
+if IPN_PATH.is_file():
+    print(f'Reading indirect names from file ({IPN_PATH})', file=sys.stderr)
+    with open(IPN_PATH, 'rt', encoding=ENCODING) as handle:
+        indirect_names = yaml.safe_load(handle)
+
+if indirect_names:
+    print(f'Indirect names from file gives map ({indirect_names})', file=sys.stderr)
+    if (the_names := indirect_names.get('packages', [])):
+        print(f'Extending indirect packages ({the_names}) from file', file=sys.stderr)
+        INDIRECT_NAMES.extend(the_names)
+        INDIRECT_NAMES = sorted(set(INDIRECT_NAMES))
+else:
+    print(f'No indirect names from file ({IPN_PATH})', file=sys.stderr)
+
 TARGET = """\
 __version__ = '$version$+parent.$revision$'\
 """
@@ -24,7 +60,7 @@ __version__ = '$version$+parent.$revision$'\
 
 @functools.lru_cache()
 def _fetch_direct_dependency_names():
-    with pathlib.Path('requirements.txt').open(encoding=ENCODING) as requirements_txt:
+    with pathlib.Path('requirements.txt').open() as requirements_txt:
         install_requires = [
             str(requirement)
             for requirement
@@ -36,43 +72,35 @@ def _fetch_direct_dependency_names():
 def _generate_dependency_information() -> None:
     """Use pip-licenses for creation of diverse databases and graphs."""
     install_requires = _fetch_direct_dependency_names()
-    tokens = set(list(string.ascii_letters + '-_'))
-    direct_names = [''.join(c for c in term if c in tokens) for term in install_requires]
+    tokens = set(list(string.ascii_letters + '-_.'))
+    direct_names = [''.join(c for c in term if c in tokens).strip('.') for term in install_requires]
+    print('Direct dependencies identified as:', file=sys.stderr)
+    for d_dep in direct_names:
+        print(f'- {d_dep}', file=sys.stderr)
     direct_vector = [
         'pip-licenses', '--format', 'json', '-p', *direct_names,
         '--with-authors', '--with-description', '--with-urls', '--with-license-file', '--with-notice-file',
+        '--with-system',  # HACK A DID ACK for setuptools
         '--output-file', str(TP_PATH / 'direct-dependency-licenses.json')]
-    noise = subprocess.run(direct_vector, capture_output=True, encoding=ENCODING, text=True, check=True).stdout.strip()  # nosec
+    noise = subprocess.run(direct_vector, capture_output=True, encoding=ENCODING, text=True).stdout.strip()  # nosec
     if not noise.startswith('created path: ') or not noise.endswith('direct-dependency-licenses.json'):
         raise RuntimeError(noise)
 
-    indirect_names = [  # TODO(sthagen) these indirect deps may diverge ...
-        'appdirs',
-        'attrs',
-        'cattrs',
-        'certifi',
-        'charset-normalizer',
-        'click',
-        'deprecated',
-        'idna',
-        'oauthlib',
-        'requests',
-        'requests-oauthlib',
-        'six',
-        'typing-extensions',
-        'url-normalize',
-        'urllib3',
-        'wrapt',
-
-    ]
     full_vector = [
-        'pip-licenses', '--format', 'json', '-p', *direct_names, *indirect_names,
+        'pip-licenses', '--format', 'json', '-p', *direct_names, *INDIRECT_NAMES,
         '--with-authors', '--with-description', '--with-urls', '--with-license-file', '--with-notice-file',
+        '--with-system',  # HACK A DID ACK for setuptools
         '--output-file', str(TP_PATH / 'all-dependency-licenses.json')]
-    noise = subprocess.run(full_vector, capture_output=True, encoding=ENCODING, text=True, check=True).stdout.strip()  # nosec
+    print('Licenses search per:', ' '.join(full_vector), file=sys.stderr)
+    noise = subprocess.run(full_vector, capture_output=True, encoding=ENCODING, text=True).stdout.strip()  # nosec
     if not noise.startswith('created path: ') or not noise.endswith('all-dependency-licenses.json'):
         raise RuntimeError(noise)
 
+    """
+    direct_deps='jmespath,jsonschema,langcodes,lazr.uri,orjson,pydantic,scooby,setuptools,typer'
+    pipdeptree --packages "${direct_deps}" --graph-output svg > docs/third-party/package-dependency-tree.svg
+    pipdeptree --packages "${direct_deps}" --json-tree --warn silence > docs/third-party/package-dependency-tree.json
+    """
     base_vector = ['pipdeptree', '--packages', ','.join(direct_names)]
     jobs = (
         (TP_PATH / 'package-dependency-tree.dot.txt', base_vector + ['--graph-output', 'dot']),
@@ -81,7 +109,7 @@ def _generate_dependency_information() -> None:
         (TP_PATH / 'package-dependency-tree.console.txt', base_vector + ['--warn', 'silence']),
     )
     for target, vector in jobs:
-        plot = subprocess.run(vector, capture_output=True, encoding=ENCODING, text=True, check=True).stdout.strip()  # nosec
+        plot = subprocess.run(vector, capture_output=True, encoding=ENCODING, text=True).stdout.strip()  # nosec
         target.write_text(plot, encoding=ENCODING)
 
 
@@ -98,7 +126,7 @@ def _markdown_table(table: List[Tuple[str, str, str, str, str]], header_labels=H
     """Create the gfm table as string."""
     columns = header_labels
     col_wid = {key: len(key) for key in columns}
-    for record in table:
+    for slot, record in enumerate(table):
         for key, cell in zip(columns, record):
             col_wid[key] = max(len(cell), col_wid[key])
 
@@ -126,7 +154,11 @@ def _extract_rows(data):
         ver_sion = f'[{ver}](https://pypi.org/project/{nam}/{ver}/)'
         lic = record['License']
         aut = record['Author']
+        if aut == 'UNKNOWN' and nam in FALLBACK_AUTHORS:
+            aut = FALLBACK_AUTHORS[nam]
         des = record['Description']
+        if des == 'UNKNOWN' and nam in FALLBACK_DESCRIPTIONS:
+            des = FALLBACK_DESCRIPTIONS[nam]
         rows.append((nam_e, ver_sion, lic, aut, des))
     rows.sort()
     return rows
@@ -135,7 +167,11 @@ def _extract_rows(data):
 def direct_dependencies_table() -> None:
     """Fill in the data from the direct dependencies."""
     _generate_dependency_information()
-    print(_markdown_table(_extract_rows(_fetch_dependencies(direct_only=True))))
+    data = _fetch_dependencies(direct_only=True)
+    print('Direct dependencies at table generation:', file=sys.stderr)
+    for d_dep in data:
+        print(f'- {d_dep["Name"]}', file=sys.stderr)
+    print(_markdown_table(_extract_rows(data)))
 
 
 def indirect_dependencies_table() -> None:
@@ -143,6 +179,10 @@ def indirect_dependencies_table() -> None:
     direct_data = _fetch_dependencies(direct_only=True)
     direct_names = tuple(record['Name'] for record in direct_data)
     indirect_only_data = [rec for rec in _fetch_dependencies(direct_only=False) if rec['Name'] not in direct_names]
+    print('Indirect dependencies at table generation:', file=sys.stderr)
+    for ind_dep in indirect_only_data:
+        print(f'- {ind_dep["Name"]}', file=sys.stderr)
+
     print(_markdown_table(_extract_rows(indirect_only_data)))
 
 
--- gen_sbom.py
+++ gen_sbom.py
@@ -4,8 +4,8 @@ __all__ = ['sbom_sha256']
 
 ENCODING = 'utf-8'
 TARGET = """\
-The [SBOM in CycloneDX v1.4 JSON format](https://github.com/sthagen/laskea/blob/default/sbom.json) with\
- SHA256 checksum ([$hash_8$ ...](https://raw.githubusercontent.com/sthagen/laskea/default/sbom.json.sha256\
+The [SBOM in CycloneDX v1.4 JSON format](https://github.com/sthagen/pilli/blob/default/sbom.json) with\
+ SHA256 checksum ([$hash_8$ ...](https://raw.githubusercontent.com/sthagen/pilli/default/sbom.json.sha256\
  "sha256:$hash_full$")).\
 """
 
